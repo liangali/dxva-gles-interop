41738c649891882bea211043113231f20bc7410a
Li, Liang A <liang.a.li@intel.com>
2023-03-23 15:46:31 +0800

    init EGL

--- gles-proc/main.cpp
+++ gles-proc/main.cpp
@@ -8,6 +8,21 @@
 #include <EGL/egl.h>
 #include <EGL/eglext.h>
 
+#ifdef _WIN64
+#define GWL_USERDATA GWLP_USERDATA
+#endif
+
+/// esCreateWindow flag - RGB color buffer
+#define ES_WINDOW_RGB           0
+/// esCreateWindow flag - ALPHA color buffer
+#define ES_WINDOW_ALPHA         1
+/// esCreateWindow flag - depth buffer
+#define ES_WINDOW_DEPTH         2
+/// esCreateWindow flag - stencil buffer
+#define ES_WINDOW_STENCIL       4
+/// esCreateWindow flat - multi-sample buffer
+#define ES_WINDOW_MULTISAMPLE   8
+
 const char *vert_src = "\
 #version 300 es\n\
 layout (location = 0) in vec3 in_position;\n\
@@ -22,19 +37,220 @@ void main()\n\
 
 const char *frag_src = "\
 #version 300 es\n\
+precision mediump float;                     \n\
 in vec2 texcoord;\n\
 out vec3 fragcolor;\n\
 uniform sampler2D tex;\n\
 void main()\n\
 {\n\
-    fragcolor = 0.5;\n\
+    fragcolor = 1.0 - texture(tex, texcoord).rgb;\n\
 }\n\
 ";
 
 // fragcolor = 1.0 - texture(tex, texcoord).rgb;\n\
 
+typedef struct
+{
+    // Handle to a program object
+    GLuint programObject;
+} UserData;
+
+struct ESContext
+{
+    void* platformData;
+    void* userData;
+    GLint width;
+    GLint height;
+    EGLNativeDisplayType eglNativeDisplay;
+    EGLNativeWindowType  eglNativeWindow;
+    EGLDisplay  eglDisplay;
+    EGLContext  eglContext;
+    EGLSurface  eglSurface;
+
+};
+
+EGLint getContextRenderableType(EGLDisplay eglDisplay)
+{
+#ifdef EGL_KHR_create_context
+    const char* extensions = eglQueryString(eglDisplay, EGL_EXTENSIONS);
+
+    // check whether EGL_KHR_create_context is in the extension string
+    if (extensions != NULL && strstr(extensions, "EGL_KHR_create_context"))
+    {
+        // extension is supported
+        return EGL_OPENGL_ES3_BIT_KHR;
+    }
+#endif
+    // extension is not supported
+    return EGL_OPENGL_ES2_BIT;
+}
+
+LRESULT WINAPI esWindowProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+    LRESULT  lRet = 1;
+    return lRet;
+}
+
+GLboolean winCreate(ESContext* esContext, const char* title)
+{
+    WNDCLASS wndclass = { 0 };
+    DWORD    wStyle = 0;
+    RECT     windowRect;
+    HINSTANCE hInstance = GetModuleHandle(NULL);
+
+    wndclass.style = CS_OWNDC;
+    wndclass.lpfnWndProc = (WNDPROC)esWindowProc;
+    wndclass.hInstance = hInstance;
+    wndclass.hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH);
+    wndclass.lpszClassName = "opengles3.0";
+
+    if (!RegisterClass(&wndclass))
+    {
+        return FALSE;
+    }
+
+    wStyle = WS_VISIBLE | WS_POPUP | WS_BORDER | WS_SYSMENU | WS_CAPTION;
+
+    // Adjust the window rectangle so that the client area has
+    // the correct number of pixels
+    windowRect.left = 0;
+    windowRect.top = 0;
+    windowRect.right = esContext->width;
+    windowRect.bottom = esContext->height;
+
+    AdjustWindowRect(&windowRect, wStyle, FALSE);
+
+    esContext->eglNativeWindow = CreateWindow(
+        "opengles3.0",
+        title,
+        wStyle,
+        0,
+        0,
+        windowRect.right - windowRect.left,
+        windowRect.bottom - windowRect.top,
+        NULL,
+        NULL,
+        hInstance,
+        NULL);
+
+    // Set the ESContext* to the GWL_USERDATA so that it is available to the ESWindowProc
+#ifdef _WIN64
+   //In LLP64 LONG is still 32bit.
+    SetWindowLongPtr(esContext->eglNativeWindow, GWL_USERDATA, (LONGLONG)(LONG_PTR)esContext);
+#else
+    SetWindowLongPtr(esContext->eglNativeWindow, GWL_USERDATA, (LONG)(LONG_PTR)esContext);
+#endif
+
+    if (esContext->eglNativeWindow == NULL)
+    {
+        return GL_FALSE;
+    }
+
+    ShowWindow(esContext->eglNativeWindow, TRUE);
+
+    return GL_TRUE;
+}
+
+int initEGL(ESContext* esContext)
+{
+    if (!winCreate(esContext, "Triangle"))
+    {
+        printf("ERROR: WinCreate failed! \n");
+        return -1;
+    }
+
+    esContext->eglDisplay = eglGetDisplay(esContext->eglNativeDisplay);
+    if (esContext->eglDisplay == EGL_NO_DISPLAY)
+    {
+        printf("ERROR: eglGetDisplay failed! \n");
+        return -1;
+    }
+
+    EGLConfig config = {};
+    EGLint majorVersion = 0;
+    EGLint minorVersion = 0;
+    EGLint contextAttribs[] = { EGL_CONTEXT_CLIENT_VERSION, 3, EGL_NONE };
+
+    // Initialize EGL
+    if (!eglInitialize(esContext->eglDisplay, &majorVersion, &minorVersion))
+    {
+        printf("ERROR: eglInitialize failed! \n");
+        return -1;
+    }
+
+    GLuint flags = ES_WINDOW_RGB;
+    EGLint numConfigs = 0;
+    EGLint attribList[] =
+    {
+       EGL_RED_SIZE,       5,
+       EGL_GREEN_SIZE,     6,
+       EGL_BLUE_SIZE,      5,
+       EGL_ALPHA_SIZE,     (flags & ES_WINDOW_ALPHA) ? 8 : EGL_DONT_CARE,
+       EGL_DEPTH_SIZE,     (flags & ES_WINDOW_DEPTH) ? 8 : EGL_DONT_CARE,
+       EGL_STENCIL_SIZE,   (flags & ES_WINDOW_STENCIL) ? 8 : EGL_DONT_CARE,
+       EGL_SAMPLE_BUFFERS, (flags & ES_WINDOW_MULTISAMPLE) ? 1 : 0,
+       // if EGL_KHR_create_context extension is supported, then we will use
+       // EGL_OPENGL_ES3_BIT_KHR instead of EGL_OPENGL_ES2_BIT in the attribute list
+       EGL_RENDERABLE_TYPE, getContextRenderableType(esContext->eglDisplay),
+       EGL_NONE
+    };
+
+    // Choose config
+    if (!eglChooseConfig(esContext->eglDisplay, attribList, &config, 1, &numConfigs))
+    {
+        printf("ERROR: eglChooseConfig failed! \n");
+        return -1;
+    }
+    if (numConfigs < 1)
+    {
+        printf("ERROR: egl numConfigs < 1 ! \n");
+        return -1;
+    }
+
+    // Create a surface
+    esContext->eglSurface = eglCreateWindowSurface(esContext->eglDisplay, config, esContext->eglNativeWindow, NULL);
+    if (esContext->eglSurface == EGL_NO_SURFACE)
+    {
+        printf("ERROR: eglCreateWindowSurface failed! \n");
+        return -1;
+    }
+
+    // Create a GL context
+    esContext->eglContext = eglCreateContext(esContext->eglDisplay, config, EGL_NO_CONTEXT, contextAttribs);
+    if (esContext->eglContext == EGL_NO_CONTEXT)
+    {
+        printf("ERROR: eglCreateContext failed! \n");
+        return -1;
+    }
+
+    // Make the context current
+    if (!eglMakeCurrent(esContext->eglDisplay, esContext->eglSurface, esContext->eglSurface, esContext->eglContext))
+    {
+        printf("ERROR: eglMakeCurrent failed! \n");
+        return -1;
+    }
+
+    return 0;
+}
+
 int main(int argc, char *argv[])
 {
+    ESContext esContextData = {};
+    ESContext* esContext = &esContextData;
+    esContext->width = 320;
+    esContext->height = 240;
+    esContext->userData = malloc(sizeof(UserData));
+
+    if (initEGL(esContext) != 0)
+    {
+        printf("ERROR: InitEGL failed! \n");
+        return -1;
+    }
+
+    printf("GL_RENDERER: %s\n", glGetString(GL_RENDERER));
+    printf("GL_VERSION: %s\n", glGetString(GL_VERSION));
+    printf("GL_SHADING_LANGUAGE_VERSION: %s\n", glGetString(GL_SHADING_LANGUAGE_VERSION));
+
     //========== shader program
     GLuint vert_shader = glCreateShader(GL_VERTEX_SHADER);
     glShaderSource(vert_shader, 1, &vert_src, NULL);
@@ -152,7 +368,7 @@ int main(int argc, char *argv[])
     glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
 
     //========== read output image
-    glReadPixels(0, 0, width, height, GL_RGB, GL_UNSIGNED_BYTE, (GLvoid *)img_out.data());
+    glReadPixels(0, 0, width, height, GL_RGB_INTEGER, GL_UNSIGNED_BYTE, (GLvoid *)img_out.data());
 
     //error = lodepng::encode("results/img_out.png", img_out, width, height, LCT_RGB);
     //if (error) std::cout << "encoder error " << error << ": " << lodepng_error_text(error) << std::endl;

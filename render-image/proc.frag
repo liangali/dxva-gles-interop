f649f3129fb7c277f732afdda544071b64bbe940
liangali <liang.a.li@intel.com>
2023-06-25 14:11:30 +0800

    use fragment shader to flip image

--- render-image/proc.frag
+++ render-image/proc.frag
@@ -5,7 +5,28 @@ in vec2 texcoord;
 out vec3 fragcolor;
 uniform sampler2D tex;
 
+vec2 Flip_v(float flip, vec2 uv);
+vec3 Flip_v(float flip, vec3 uv);
+vec4 Flip_v(float flip, vec4 uv);
+
+vec2 Flip_v(float flip, vec2 uv) {
+    if(flip > 0.5)
+        uv.y = 1.0 - uv.y;
+    return uv;
+}
+vec3 Flip_v(float flip, vec3 uv) {
+    if(flip > 0.5)
+        uv.y = 1.0 - uv.y;
+    return uv;
+}
+vec4 Flip_v(float flip, vec4 uv) {
+    if(flip > 0.5)
+        uv.y = 1.0 - uv.y;
+    return uv;
+}
+
 void main()
 {
-    fragcolor = texture(tex, texcoord).rgb;
+    fragcolor = texture(tex, Flip_v(1.0,texcoord)).rgb;
+    fragcolor = fragcolor * min(1.0, 1.0);
 }
--- render-image/render_image.cpp
+++ render-image/render_image.cpp
@@ -413,10 +413,10 @@ int initResources(ESContext* ctx, const char* imgFile)
 {
     float vertices[] = {
         // positions          // colors           // texture coords
-         1.0f,  1.0f, 0.0f,   1.0f, 0.0f, 0.0f,   0.0f, 0.0f, // top right
-         1.0f, -1.0f, 0.0f,   0.0f, 1.0f, 0.0f,   0.0f, 1.0f, // bottom right
-        -1.0f, -1.0f, 0.0f,   0.0f, 0.0f, 1.0f,   1.0f, 1.0f, // bottom left
-        -1.0f,  1.0f, 0.0f,   1.0f, 1.0f, 0.0f,   1.0f, 0.0f  // top left 
+         1.0f,  1.0f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f, // top right
+         1.0f, -1.0f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f, // bottom right
+        -1.0f, -1.0f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f, // bottom left
+        -1.0f,  1.0f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f  // top left 
     };
 
     unsigned int indices[] = {
